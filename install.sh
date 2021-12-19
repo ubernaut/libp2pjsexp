b38ad402484d6588d9ac7b3d4f6f3cc96034c065
cos <collin.schroeder@gmail.com>
2021-12-19 08:54:35 -0900

    install node and docker

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/#!/usr/bin/env bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh ./get-docker.sh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+node --version
